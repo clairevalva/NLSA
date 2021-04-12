2b95cd4d726d9af2492981fc946005ebd9709130
Claire Valva <claire.valva@gmail.com>
2021-04-12 10:59:34 -0400

    some plots, plotting files, .gitignore

---
+++ examples/blocking/plotting_scripts/test_plot.m
@@ -0,0 +1,12 @@
+
+% x     =getData(model.srcComponent); 
+toload   =  getProjectedData( model );
+test = reshape(toload(:,1), [],20);
+% test = reshape(x, 41, [], 240);
+
+
+figure('position', [1, 1, 300, 300])
+%contourf(squeeze(test(:,3,:)))
+contourf(squeeze(test))
+colorbar;
+print('-dpng','-r300',"/kontiki6/cnv5172/NLSA/examples/blocking/figs/test_projection.png")
