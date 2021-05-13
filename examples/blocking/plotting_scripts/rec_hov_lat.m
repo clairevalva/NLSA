45e7b1107c04c1eb572769ec4274f8115fc102e2
clairevalva <clairev@nyu.edu>
2021-05-13 09:51:43 -0400

    minor changes

--- examples/blocking/plotting_scripts/rec_hov_lat.m
+++ examples/blocking/plotting_scripts/rec_hov_lat.m
@@ -1,8 +1,8 @@
 function f = rec_hov_lat(wantrec, latind, totall)
 
     % want rec is the reconstruction I want to look at
-    batch_list = [96, 48]; % also 48, 36??
-    save_pre = "/kontiki6/cnv5172/NLSA/examples/blocking/figs/reconstructions/rec_hovs/hov_rec_phi_"
+    batch_list = [48, 96]; % also 48, 36??
+    save_pre = "/scratch/cnv5172/NLSA/examples/blocking/figs/reconstructions/test2_hov_rec_phi_"
     
     % get grid and weights
     makegrid; % will just want lon from this but that is ok
@@ -10,13 +10,14 @@ function f = rec_hov_lat(wantrec, latind, totall)
 
     rrr = wantrec;
 
-    savename = save_pre + string(wantrec) + "_lat_" + string(30 + 1.5*latind) + ".png"
+    savename = save_pre + string(wantrec) + "_lat_" + string(30 + 1.5*latind) + ".png";
+    savename_spectra = "/scratch/cnv5172/NLSA/examples/blocking/figs/spectra/spectra_phi_" + string(wantrec) + "_lat_" + string(30 + 1.5*latind) + ".png";
     % pull the reconstructed data
     get_model_changephi;
-    plot_arr = reshape(newx, 41, [], 240);
+    arr = reshape(newx, 41, 240, []);
 
-    plot_arr = squeeze(plot_arr(latind, :, :))
-
+    plot_arr = transpose(squeeze(arr(latind,:,:)));
+    size(plot_arr)
     
 
     startdate = datetime(2009,01, 01); 
@@ -29,20 +30,48 @@ function f = rec_hov_lat(wantrec, latind, totall)
     unw = (reshape(w, 240, 41).^-1);
     
     lim_pos = max(abs(plot_arr), [], "all");
+    size(unw(:,latind))
+    size(plot_arr)
+    
+    formatted = unw(:,latind).*transpose(plot_arr);
+    formatted = transpose(formatted);
 
-    formatted = plot_arr.*unw(:,latind)
+    timefreq = fftshift(fftfreq(size(arr,2), 0.25));
+    spacefreq = fftshift(fftfreq(240, 1/240)); %i.e. wavenumber
+    transformed = fftn(formatted);
 
+
+    % make the hov
     figure('position', [1, 1, 500, 1000]);
     caxis manual
     colormap jet
     caxis([-1*lim_pos lim_pos]);
-    contourf(lon, datenum(dts(1:totall)), formatted(1:totall,:), 30,'LineColor', 'none')
+    contourf(lon, datenum(dts(1:totall)), formatted(1:totall, :), 30,'LineColor', 'none')
     h = colorbar;
     xlabel("deg E")
     ylabel("date")
     ylabel(h, 'z500 comp (m/s)');
-    
+    yt = yticks;
+    yticklabels(datestr(yt));
     print('-dpng','-r500',savename)
     close
+
+
+
+    % also plot the spectra
+    figure('position', [1, 1, 1000, 600])
+    contourf(timefreq,spacefreq, fftshift(log(abs(transformed))), 30,'LineColor', 'none')
+    %colormap(flipud(pink))
+    colormap(jet)
+    ylabel("frequency (day^-1)")
+    xlabel("wavenumber")
+    xlim([-120, 120])
+    ylim([0, 0.5])
+    title("rec with phi " + string(wantrec))
+
+    h = colorbar;
+    ylabel(h, 'power');
+    print('-dpng','-r500',savename_spectra);
+    close;
 
 end
